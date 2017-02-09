0f8a6cad94c1e989612d1ded4ed386eca603bc7f
An Doan <anndoan@compute00.cluster.pvt>
2017-02-08 22:42:26 -0700

    testing tesla

--- answers.sh
+++ answers.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/env bash 
 
 datasets='/vol2/home/anndoan/data-sets/'
-# Which state has the highest popultion?
+# Which state has the lowest murder rate?
 
 states="$datasets/misc/states.tab.gz"
 
 answer_1=$(zcat $states \
-    | grep -v '^#' \
-    | cut -f1,2 \
-    | sort -k2n \
+    | cut -f1,6 \
+    | sort -k2g \
+    | head -n2 \
     | tail -n1 \
     | cut -f1 \
-    | sed 's/"//g') 
-
+    | sed 's/"//g')
+   
 echo "answer-1: $answer_1"
 
 sample="$datasets/fasta/sample.fa"
